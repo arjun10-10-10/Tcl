27f3f94a12ba005d118c0b7db5682a6d26133123
ARJUN R <arjunravi2134@gmail.com>
2026-07-13 11:28:02 +0530

    Create 2_leapyear.tcl

---
+++ Practice/2_leapyear.tcl
@@ -0,0 +1,11 @@
+proc isLeapYear {year} {
+    if {$year % 400 == 0} {
+        return true
+    } elseif {$year % 100 == 0} {
+        return false
+    } elseif {$year % 4 == 0} {
+        return true
+    } else {
+        return false
+    }
+}
